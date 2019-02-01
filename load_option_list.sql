54d0d6b82c0e4a72a0f444d0a6a67a99bb518fbb
Roman Sigalov <sigalovroman@gmail.com>
2019-02-01 11:45:04 -0500

    Updated SQL script to allow for different option databases

--- load_option_list.sql
+++ load_option_list.sql
@@ -1,18 +1,19 @@
-with security_table as (
+    with security_table as (
     select
         secid, date, close as under_price
     from optionm.SECPRD
     where secid = _secid_
+    and date >= _start_date_
+    and date <= _end_date_
 )
-(
-    select
-	   o.secid, o.symbol, o.date, o.exdate, o.cp_flag, o.strike_price, o.impl_volatility,
+(   select
+        o.secid, o.symbol, o.date, o.exdate, o.cp_flag, o.strike_price, o.impl_volatility,
        (o.best_offer + o.best_bid)/2 as mid_price,
        s.under_price
-	from OPTIONM.OPPRCD2017 as o
+        from _data_base_ as o
     left join security_table as s
         on o.secid = s.secid and o.date = s.date
-	where o.secid = _secid_
+        where o.secid = _secid_
     and o.cp_flag = 'C'
     and o.open_interest > 0
     and o.best_bid > 0
@@ -24,16 +25,18 @@ with security_table as (
     and (o.best_offer + o.best_bid)/2 < s.under_price
     and o.exdate - o.date < 60
     and o.exdate - o.date > 25
-	order by o.exdate, o.strike_price
+    and o.date >= _start_date_
+    and o.date <= _end_date_
+    order by o.exdate, o.strike_price
 ) union (
-	select
-	   o.secid, o.symbol, o.date, o.exdate, o.cp_flag, o.strike_price, o.impl_volatility,
+    select
+       o.secid, o.symbol, o.date, o.exdate, o.cp_flag, o.strike_price, o.impl_volatility,
        (o.best_offer + o.best_bid)/2 as mid_price,
        s.under_price
-	from OPTIONM.OPPRCD2017 as o
+    from _data_base_ as o
     left join security_table as s
         on o.secid = s.secid and o.date = s.date
-	where o.secid = _secid_
+    where o.secid = _secid_
     and o.cp_flag = 'P'
     and o.open_interest > 0
     and o.best_bid > 0
@@ -46,5 +49,7 @@ with security_table as (
     and (o.best_offer + o.best_bid)/2 >= GREATEST(0, o.strike_price/1000 - s.under_price)
     and o.exdate - o.date < 60
     and o.exdate - o.date > 25
-	order by o.exdate, o.strike_price
+    and o.date >= _start_date_
+    and o.date <= _end_date_
+    order by o.exdate, o.strike_price
 )

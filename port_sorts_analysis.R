836e03e339403529af01803fd841ee463f5b4312
Roman Sigalov <sigalovroman@gmail.com>
2019-08-18 10:23:32 -0400

    General update

--- port_sorts_analysis.R
+++ port_sorts_analysis.R
@@ -3,192 +3,195 @@ library("data.table")
 library("dplyr")
 library("reshape")
 library("readr")
+library("stargazer")
 setwd("~/Documents/PhD/disaster-risk-revision/")
 
-# Loading returns on portfolios sorted by individual disaster measure:
-ret_ind <- read_csv("estimated_data/ind_disaster_sorts/port_sort_ret.csv") %>%
-  dplyr::rename(date = X1)
-ret_agg <- read_csv("estimated_data/ind_disaster_sorts/port_sort_agg_ret.csv") %>%
-  dplyr::rename(date = X1)
+############################################################
+# Comparing loadings on fctors for different maturities:
+############################################################
+reg_res <- read_csv("estimated_data/disaster_sorts/reg_results_agg.csv")
+reg_res_filter <- reg_res %>%
+  filter(level %in% c("union_cs"), variable %in% c("D_clamp"), port == "ew_diff")
 
-bm_ind <- read_csv("estimated_data/ind_disaster_sorts/port_sort_bm.csv") %>%
-  dplyr::rename(date = X1)
-bm_agg <- read_csv("estimated_data/ind_disaster_sorts/port_sort_agg_bm.csv") %>%
-  dplyr::rename(date = X1)
-
-op_ind <- read_csv("estimated_data/ind_disaster_sorts/port_sort_op.csv") %>%
-  dplyr::rename(date = X1)
-op_agg <- read_csv("estimated_data/ind_disaster_sorts/port_sort_agg_op.csv") %>%
-  dplyr::rename(date = X1)
-
-####################################################################
-# Comparing averages
-####################################################################
-ret_ind_means <- ret_ind %>%
-  filter(days %in% c(30,60,120,180)) %>%
-  select(days, variable, vw_1, vw_2, vw_3, vw_4, vw_5) %>%
-  group_by(variable, days) %>%
-  summarize_all(mean, na.rm = T)
-
-ret_agg_means <- ret_agg %>%
-  select(days, variable, vw_1, vw_2, vw_3, vw_4, vw_5) %>%
-  group_by(variable, days) %>%
-  summarize_all(mean, na.rm = T)
-
-# Combining them together:
-convert_for_plotting <- function(df, port_type_str) {
-  # Subsetting columns
-  df <- df %>%
-    filter(days %in% c(30,60,120,180)) %>%
-    filter(variable %in% c("D_clamp", "rn_prob_20", "rn_prob_80"))
+# Adding standard error bands
+reg_res_ci <- reg_res_filter %>%
+  mutate(alpha = alpha_se*2, beta_MKT = beta_MKT_se*2, beta_SMB = beta_SMB_se*2,
+         beta_HML = beta_HML_se*2, beta_CMA=beta_CMA_se*2, beta_RMW=beta_RMW_se*2) %>%
+  select(FF, days, alpha, beta_MKT, beta_SMB, beta_HML, beta_CMA, beta_RMW) %>%
+  dplyr::rename(MKT = beta_MKT, HML = beta_HML, SMB = beta_SMB, CMA = beta_CMA, RMW = beta_RMW) %>%
+  data.table() %>% melt(id.vars = c("days", "FF")) %>%
+  dplyr::rename(ci = value)
   
-  # Picking the right columns according to portfolio type specified
-  if (port_type_str == "VW") {
-    df <- df %>% select(days, variable, vw_1, vw_2, vw_3, vw_4, vw_5)
-  } else if (port_type_str == "EW") {
-    df <- df %>% select(days, variable, ew_1, ew_2, ew_3, ew_4, ew_5)
-  } else {
-    stop(paste("No portfolio type", port_type_str))
-  }
-  
-  # Melting the dataframes
-  df %>%
-    group_by(variable, days) %>%
-    mutate_all(function(x) x*12) %>%
-    summarize_all(mean, na.rm = T) %>%
-    data.table() %>% melt(id.vars = c("days", "variable")) %>%
-    dplyr::rename(port = variable.1) %>% # renaming variable for portfolios
-    mutate(port = as.numeric(substr(port, 4, 4))) %>%  # Converting portfolio names to numbers
-    mutate(port_type = port_type_str) %>% data.table()
-}
+df_to_plot <-reg_res_filter %>%
+  select(FF, days, alpha, beta_MKT, beta_SMB, beta_HML, beta_CMA, beta_RMW) %>%
+  dplyr::rename(MKT = beta_MKT, HML = beta_HML, SMB = beta_SMB, CMA = beta_CMA, RMW = beta_RMW) %>%
+  data.table() %>% melt(id.vars = c("days", "FF")) %>%
+  left_join(reg_res_ci, by = c("days", "FF", "variable"))
 
-comp_port_plots <- function(df_ind, df_agg, port_type_str) {
-  ret_melt_ew_ind <- convert_for_plotting(df_ind, port_type_str) %>%
-    mutate(sorting_var_type = "Individual")
-  
-  # Renaming portfolios for beta sorts: call the most exposed to disaster as 5 and least as 1
-  ret_melt_ew_agg <- convert_for_plotting(df_agg, port_type_str) %>%
-    mutate(port = 6 - port) %>%
-    mutate(sorting_var_type = "Beta")
-  
-  ggplot(rbind(ret_melt_ew_agg, ret_melt_ew_ind), aes(x = port, y = value, color = sorting_var_type)) +
-    geom_line() +
-    facet_grid(variable ~ days)
-}
+ggplot(df_to_plot, aes(x = factor(FF), y = value, fill = factor(days))) +
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=value-ci, ymax=value+ci),width=.1, alpha = 0.6, position=position_dodge(.9)) +
+  facet_grid(variable ~ ., scales = "free_y") +
+  scale_fill_brewer(palette="Paired", name = "Days") + theme_minimal() +
+  geom_hline(yintercept = 0, size = 0.2) +
+  xlab("Factors included") + ylab("")
 
-comp_port_plots(ret_ind, ret_agg, "VW")
-comp_port_plots(ret_ind, ret_agg, "EW")
 
-comp_port_plots(bm_ind, bm_agg, "VW")
-comp_port_plots(bm_ind, bm_agg, "EW")
+# Now looking at market-to-book and op. profitability:
+bm <- read_csv("estimated_data/disaster_sorts/port_sort_bm_agg.csv") %>% 
+  dplyr::rename(date = X1)
+op <- read_csv("estimated_data/disaster_sorts/port_sort_op_agg.csv") %>%
+  dplyr::rename(date = X1)
 
-comp_port_plots(op_ind, op_agg, "VW")
-comp_port_plots(op_ind, op_agg, "EW")
-
-# Generating tables:
-ret_ind %>%
-  select(-c(date, ew_count, vw_count)) %>%
+# Averaging:
+bm_av <- bm %>%
   group_by(variable, days) %>%
-  summarize_all(mean, na.rm = T)
+  summarize_all(mean) %>% ungroup()
 
-####################################################################
-# Looking at time series
-####################################################################
-port_type_str = "EW"
-df_to_plot <- ret_agg %>%
-  filter(days %in% c(30,60,120,180)) %>%
-  filter(variable %in% c("D_clamp", "rn_prob_20", "rn_prob_80"))
-  if (port_type_str == "VW") {
-    df_to_plot <- df_to_plot %>% select(date, days, variable, vw_1, vw_2, vw_3, vw_4, vw_5)
-  } else if (port_type_str == "EW") {
-    df_to_plot <- df_to_plot %>% select(date, days, variable, ew_1, ew_2, ew_3, ew_4, ew_5)
-  } else {
-    stop(paste("No portfolio type", port_type_str))
-  }
-df_to_plot <- df_to_plot %>%
-  data.table() %>% melt(id.vars = c("date", "days", "variable")) %>%
-  dplyr::rename(port = variable.1) %>% # renaming variable for portfolios
-  mutate(port = as.numeric(substr(port, 4, 4))) %>%  # Converting portfolio names to numbers
-  mutate(port_type = port_type_str) %>% 
-  arrange(variable, days, port, date) %>%
-  group_by(variable, days, port) %>%
-  mutate(value = value + 1) %>%
-  mutate(position = cumprod(value))
-
-ggplot(df_to_plot %>% filter(port %in% c(1,5)), aes(x = date, y = position, color = factor(port))) +
-  geom_line() +
-  facet_grid(variable ~ days)
-         
-
-
-
-
-
-ret_ind %>%
-  filter(days %in% c(30,60,120,180)) %>%
-  mutate(diff = 12*(ew_5 - ew_1)) %>%
+op_av <- op %>%
   group_by(variable, days) %>%
-  summarize(mean = mean(diff))
+  summarize_all(mean) %>% ungroup()
 
-ret_agg %>%
-  mutate(diff = 12*(ew_1 - ew_5)) %>%
+# Melting, merging and prepare for plotting:
+bm_av_melt <- bm_av %>% 
+  filter(variable == "D_clamp") %>%
+  select(days, ew_1, ew_2, ew_3, ew_4, ew_5) %>%
+  data.table() %>% melt(id.vars = "days") %>%
+  mutate(char = "Book-to-Market")
+
+op_av_melt <- op_av %>% 
+  filter(variable == "D_clamp") %>%
+  select(days, ew_1, ew_2, ew_3, ew_4, ew_5) %>%
+  data.table() %>% melt(id.vars = "days") %>%
+  mutate(char = "Operating Profitability")
+
+av_melt <- rbind(bm_av_melt,op_av_melt)
+
+# Plotting
+ggplot(av_melt, aes(x = factor(days), y = value, fill = variable)) +
+  geom_bar(position=position_dodge(), stat="identity") +
+  facet_grid(char ~ ., scales = "free_y") +
+  scale_fill_brewer(palette="Paired", name = "Portfolio") + theme_minimal()
+
+ggplot(av_melt %>% mutate(variable = as.numeric(substr(variable, 4,4))), 
+       aes(x = variable, y = value)) +
+  geom_point() + geom_line() +
+  facet_grid(char ~ days, scales = "free_y") +
+  theme_minimal() + xlab("Portfolio") + ylab("")
+
+# Plotting time series of operating profitability and
+# book to market at formation for ew_1 and ew_2:
+bm_melt <- bm %>%
+  filter(variable == "D_clamp", level == "union_cs", days == -99) %>%
+  select(date, days, ew_1, ew_5) %>%
+  data.table() %>% melt(id.vars = c("date", "days")) %>%
+  mutate(char = "Book-to-Market")
+
+op_melt <- op %>%
+  filter(variable == "D_clamp", level == "union_cs", days == -99) %>%
+  select(date, days, ew_1, ew_5) %>%
+  data.table() %>% melt(id.vars = c("date", "days")) %>%
+  mutate(char = "Operating Profitability")
+
+rbind(bm_melt, op_melt) %>%
+  ggplot(aes(x = date, y=value, color = variable)) +
+  geom_line() + facet_grid(char~., scales = "free_y") +
+  xlab("") + ylab("") + theme_minimal() +
+  scale_x_date(date_breaks = "2 years", date_labels = "%Y")
+
+
+
+############################################################
+# Comparing loadings and characteristics for sorts on 
+# individual companies
+############################################################
+reg_res <- read_csv("estimated_data/disaster_sorts/reg_results_ind.csv")
+reg_res_filter <- reg_res %>%
+  filter(variable %in% c("D_clamp"), port == "ew_diff")
+
+# Adding standard error bands
+reg_res_ci <- reg_res_filter %>%
+  mutate(alpha = alpha_se*2, beta_MKT = beta_MKT_se*2, beta_SMB = beta_SMB_se*2,
+         beta_HML = beta_HML_se*2, beta_CMA=beta_CMA_se*2, beta_RMW=beta_RMW_se*2) %>%
+  select(FF, days, alpha, beta_MKT, beta_SMB, beta_HML, beta_CMA, beta_RMW) %>%
+  dplyr::rename(MKT = beta_MKT, HML = beta_HML, SMB = beta_SMB, CMA = beta_CMA, RMW = beta_RMW) %>%
+  data.table() %>% melt(id.vars = c("days", "FF")) %>%
+  dplyr::rename(ci = value)
+
+df_to_plot <-reg_res_filter %>%
+  select(FF, days, alpha, beta_MKT, beta_SMB, beta_HML, beta_CMA, beta_RMW) %>%
+  dplyr::rename(MKT = beta_MKT, HML = beta_HML, SMB = beta_SMB, CMA = beta_CMA, RMW = beta_RMW) %>%
+  data.table() %>% melt(id.vars = c("days", "FF")) %>%
+  left_join(reg_res_ci, by = c("days", "FF", "variable"))
+
+ggplot(df_to_plot, aes(x = factor(FF), y = value, fill = factor(days))) +
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=value-ci, ymax=value+ci),width=.1, alpha = 0.6, position=position_dodge(.9)) +
+  facet_grid(variable ~ ., scales = "free_y") +
+  scale_fill_brewer(palette="Paired", name = "Days") + theme_minimal() +
+  geom_hline(yintercept = 0, size = 0.2) +
+  xlab("Factors included") + ylab("")
+
+
+# Now looking at market-to-book and op. profitability:
+bm <- read_csv("estimated_data/disaster_sorts/port_sort_bm_ind.csv") %>% 
+  dplyr::rename(date = X1)
+op <- read_csv("estimated_data/disaster_sorts/port_sort_op_ind.csv") %>%
+  dplyr::rename(date = X1)
+
+# Averaging:
+bm_av <- bm %>%
   group_by(variable, days) %>%
-  summarize(mean = mean(diff))
+  summarize_all(mean) %>% ungroup()
 
-# Comparing return on HML strategies for individually 
-# vs aggregate exposure
-ret_agg %>%
-  filter(days %in% c(30, 180)) %>%
-  select(-c(vw_count, ew_count)) %>%
-  select(-c(vw_1, vw_2, vw_3, vw_4, vw_5)) %>%
-  mutate(diff = ew_5 - ew_1) %>%
-  data.table() %>%
-  melt(id.vars = c("date", "variable", "days")) %>%
-  dplyr::rename(port = variable.1) %>%
-  mutate(port = substr(port, 4, 4)) %>%
-  as_tibble() %>%
-  mutate(value = value + 1) %>%
-  arrange(variable, days, date) %>%
-  group_by(variable, days, port) %>%
-  mutate(position = cumprod(value)) %>%
-  ggplot(aes(x = date, y = position, color = port)) +
-  geom_line() +
-  facet_grid(variable~days)
-
-
-
-
-ret_ind <- read_csv("estimated_data/ind_disaster_sorts/port_sort_op.csv")
-ret_agg <- read_csv("estimated_data/ind_disaster_sorts/port_sort_agg_op.csv")
-ret_ind %>% 
-  select(days, variable, ew_1, ew_2, ew_3, ew_4, ew_5) %>%
+op_av <- op %>%
   group_by(variable, days) %>%
-  summarize_all(mean, na.rm = T) %>%
-  data.table() %>%
-  melt(id.vars = c("variable", "days")) %>%
-  ggplot(aes(x = variable.1, y = value, color = days)) +
-  geom_line() + facet_wrap(~variable)
+  summarize_all(mean) %>% ungroup()
 
-ret_df %>% 
-  select(days, variable, vw_1, vw_2, vw_3, vw_4, vw_5) %>%
-  group_by(variable, days) %>%
-  summarize_all(mean, na.rm = T) %>%
-  data.table() %>%
-  melt(id.vars = c("variable", "days")) %>%
-  ggplot(aes(x = variable.1, y = value, color = days)) +
-  geom_line() + facet_wrap(~variable)
+# Melting, merging and prepare for plotting:
+bm_av_melt <- bm_av %>% 
+  filter(variable == "D_clamp") %>%
+  select(days, ew_1, ew_2, ew_3, ew_4, ew_5) %>%
+  data.table() %>% melt(id.vars = "days") %>%
+  mutate(char = "Book-to-Market")
 
-ret_df %>%
-  filter(days == -99) %>%
-  select(ew_1, ew_2, ew_3, ew_4, ew_5) %>%
-  summarize_all(mean, na.rm = T)
+op_av_melt <- op_av %>% 
+  filter(variable == "D_clamp") %>%
+  select(days, ew_1, ew_2, ew_3, ew_4, ew_5) %>%
+  data.table() %>% melt(id.vars = "days") %>%
+  mutate(char = "Operating Profitability")
 
-ret_df %>%
-  filter(days == -99) %>%
-  select(ew_1, ew_2, ew_3, ew_4, ew_5) %>%
-  summarize_all(mean, na.rm = T)
+av_melt <- rbind(bm_av_melt,op_av_melt)
+
+# Plotting
+ggplot(av_melt, aes(x = factor(days), y = value, fill = variable)) +
+  geom_bar(position=position_dodge(), stat="identity") +
+  facet_grid(char ~ ., scales = "free_y") +
+  scale_fill_brewer(palette="Paired", name = "Portfolio") + theme_minimal()
+
+ggplot(av_melt %>% mutate(variable = as.numeric(substr(variable, 4,4))), 
+       aes(x = variable, y = value)) +
+  geom_point() + geom_line() +
+  facet_grid(char ~ days, scales = "free_y") +
+  theme_minimal() + xlab("Portfolio") + ylab("")
 
 
+# Plotting time series of operating profitability and
+# book to market at formation for ew_1 and ew_2:
+bm_melt <- bm %>%
+  filter(variable == "D_clamp", days == -99) %>%
+  select(date, days, ew_1, ew_5) %>%
+  data.table() %>% melt(id.vars = c("date", "days")) %>%
+  mutate(char = "Book-to-Market")
 
+op_melt <- op %>%
+  filter(variable == "D_clamp", days == -99) %>%
+  select(date, days, ew_1, ew_5) %>%
+  data.table() %>% melt(id.vars = c("date", "days")) %>%
+  mutate(char = "Operating Profitability")
 
-
+rbind(bm_melt, op_melt) %>%
+  ggplot(aes(x = date, y=value, color = variable)) +
+  geom_line() + facet_grid(char~., scales = "free_y") +
+  xlab("") + ylab("") + theme_minimal() +
+  scale_x_date(date_breaks = "2 years", date_labels = "%Y")

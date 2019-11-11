66f7b26f15ba2cf91b86e59629468dbe20558c88
Roman Sigalov <sigalovroman@gmail.com>
2019-11-11 11:29:26 -0500

    General update

--- ind_port_sorts.py
+++ ind_port_sorts.py
@@ -4,7 +4,7 @@ for different number of days and variables. It combines EW and VW returns,
 EW and VW Book-to-Market, EW and VW Operating Profitability for different
 maturities and measures into one file and saves them to
 
-	estimated_data/ind_disaster_sorts/
+	estimated_data/disaster_sorts/
 
 folder
 """
@@ -17,115 +17,143 @@ import pandas as pd
 import wrds
 import crsp_comp
 import data_funcs
+from functools import reduce
 
-# def main(argv=None):
-# Settin up the connection to load CRSP and Compustat data
-db = wrds.Connection()
-# Main variables
-variable_list = ["D_clamp", "rn_prob_20", "rn_prob_80"]
-days_list = [30,60,90,120,150,180]
-ncuts = 5
+def main(argv=None):
+	# Settin up the connection to load CRSP and Compustat data
+	db = wrds.Connection()
+	# Main variables
+	variable_list = ["D_clamp", "rn_prob_20", "rn_prob_80"]
+	days_list = [30,60,90,120,150,180]
+	ncuts = 5
 
-# Loading data on CS interpolated D that is already merged with CRSP PERMNO.
-# Filtering companies to have at leat 5/10 observations to include in a
-# portfolio
-int_d_5 = pd.DataFrame(
-	columns = ["secid", "permno", "date", "days"] + variable_list)
-int_d = pd.DataFrame(
-	columns = ["secid", "permno", "date", "days"] + variable_list)
+	# Loading data on CS interpolated D that is already merged with CRSP PERMNO.
+	# Filtering companies to have at leat 5/10 observations to include in a
+	# portfolio
+	int_d_5 = pd.DataFrame(
+		columns = ["secid", "permno", "date", "days"] + variable_list)
+	int_d = pd.DataFrame(
+		columns = ["secid", "permno", "date", "days"] + variable_list)
 
-for i, days in enumerate(days_list):
-    print("Loading data on %d days variable" % days)
-    print("")
-    int_d_tmp = pd.read_csv("estimated_data/interpolated_D/int_ind_disaster_union_cs_" + str(days) + ".csv")
-    int_d_tmp = int_d_tmp[int_d_tmp.permno.notnull()]
-    int_d_tmp["days"] = days
-    int_d = int_d.append(int_d_tmp)
-    
-    int_d_tmp = data_funcs.filter_min_obs_per_month(int_d_tmp, "D_clamp", 5)
-    int_d_tmp["days"] = days
-    int_d_5 = int_d_5.append(int_d_tmp)
+	for i, days in enumerate(days_list):
+	    print("Loading data on %d days variable" % days)
+	    print("")
+	    int_d_tmp = pd.read_csv("estimated_data/interpolated_D/int_ind_disaster_union_cs_" + str(days) + ".csv")
+	    int_d_tmp = int_d_tmp[int_d_tmp.permno.notnull()]
+	    int_d_tmp["days"] = days
+	    int_d = int_d.append(int_d_tmp)
 
-# Filtering the series for the aggregated measure by requiring a company
-# to have at least 10 observations a month:
-print("Filtering and Averaging the data")
-print("")
-int_d_10 = data_funcs.filter_min_obs_per_month(int_d, "D_clamp", 10)
+	    int_d_tmp = data_funcs.filter_min_obs_per_month(int_d_tmp, "D_clamp", 5)
+	    int_d_tmp["days"] = days
+	    int_d_5 = int_d_5.append(int_d_tmp)
 
-# Averaging on (1) secid-month-days level and (2) secid-month level:
-d_mean_mon_days = int_d_5.groupby(["secid", "date_eom", "days"])[variable_list].mean()
-d_mean_mon = int_d_10.groupby(["secid", "date_eom"])[variable_list].mean()
+	# Filtering the series for the aggregated measure by requiring a company
+	# to have at least 10 observations a month:
+	print("Filtering and Averaging the data")
+	print("")
+	int_d_10 = data_funcs.filter_min_obs_per_month(int_d, "D_clamp", 10)
 
-# Getting last permno for a given month:
-permno_last_5 = int_d_5.groupby(["secid", "date_eom", "days"])["permno"].last()
-permno_last_10 = int_d_10.groupby(["secid", "date_eom"])["permno"].last()
+	# Averaging on (1) secid-month-days level and (2) secid-month level:
+	d_mean_mon_days = int_d_5.groupby(["secid", "date_eom", "days"])[variable_list].mean()
+	d_mean_mon = int_d_10.groupby(["secid", "date_eom"])[variable_list].mean()
 
-# Merging permno to disaster measure:
-d_mean_mon_days = pd.merge(
-	d_mean_mon_days, permno_last_5, left_index = True, right_index = True).reset_index()
-d_mean_mon = pd.merge(
-	d_mean_mon, permno_last_10, left_index = True, right_index = True).reset_index()
+	# Getting last permno for a given month:
+	permno_last_5 = int_d_5.groupby(["secid", "date_eom", "days"])["permno"].last()
+	permno_last_10 = int_d_10.groupby(["secid", "date_eom"])["permno"].last()
 
-d_mean_mon_days = d_mean_mon_days.rename(columns = {"date_eom":"date"})
-d_mean_mon = d_mean_mon.rename(columns = {"date_eom":"date"})
+	# Merging permno to disaster measure:
+	d_mean_mon_days = pd.merge(
+		d_mean_mon_days, permno_last_5, left_index = True, right_index = True).reset_index()
+	d_mean_mon = pd.merge(
+		d_mean_mon, permno_last_10, left_index = True, right_index = True).reset_index()
 
-print("Starting to form portfolios")
-print("")
-# Setting up output dataset to append data as we go
-columns = ["days", "variable"]
-columns = columns + ["ew_count"] + ["ew_" + str(x+1) for x in range(ncuts)]
-columns = columns + ["vw_count"] + ["vw_" + str(x+1) for x in range(ncuts)]
-port_sort_ret = pd.DataFrame(columns = columns)
-port_sort_bm = pd.DataFrame(columns = columns)
-port_sort_op = pd.DataFrame(columns = columns)
+	d_mean_mon_days = d_mean_mon_days.rename(columns = {"date_eom":"date"})
+	d_mean_mon = d_mean_mon.rename(columns = {"date_eom":"date"})
 
-# First do the portfolios for measures separated by days:
-db = wrds.Connection()
+	print("Starting to form portfolios")
+	print("")
+	# Setting up output dataset to append data as we go
+	columns = ["days", "variable"]
+	columns = columns + ["ew_count"] + ["ew_" + str(x+1) for x in range(ncuts)]
+	columns = columns + ["vw_count"] + ["vw_" + str(x+1) for x in range(ncuts)]
+	port_sort_ret = pd.DataFrame(columns = columns)
+	port_sort_bm = pd.DataFrame(columns = columns)
+	port_sort_op = pd.DataFrame(columns = columns)
 
-for days in days_list:
-	print("Doing portfolios sorts on %d days variable" % days)
-	ports = crsp_comp.monthly_portfolio_sorts(
-		db, d_mean_mon_days[d_mean_mon_days.days == days], variable_list, ncuts)
-	# Appending observations to output dataframe
+	# List to save constituents data:
+	constituents_df_list = []
+
+	# First do the portfolios for measures separated by days:
+	db = wrds.Connection()
+
+	for days in days_list:
+		print("Doing portfolios sorts on %d days variable" % days)
+		ports = crsp_comp.monthly_portfolio_sorts(
+			db, d_mean_mon_days[d_mean_mon_days.days == days], variable_list, ncuts)
+		# Appending observations to output dataframe
+		for variable in variable_list:
+			to_append = ports[variable]["ret"]
+			to_append["days"] = days
+			to_append["variable"] = variable
+			port_sort_ret = port_sort_ret.append(to_append)
+
+			to_append = ports[variable]["bm"]
+			to_append["days"] = days
+			to_append["variable"] = variable
+			port_sort_bm = port_sort_bm.append(to_append)
+
+			to_append = ports[variable]["op"]
+			to_append["days"] = days
+			to_append["variable"] = variable
+			port_sort_op = port_sort_op.append(to_append)
+
+			to_append_const = ports[variable]["constituents"]
+			to_append_const["days"] = days
+			to_append_const["variable"] = variable
+			to_append_const.columns = ["form_date", "permno", "port", "days", "variable"]
+			constituents_df_list.append(to_append_const)
+
+
+	# Second, do the portfolios for aggregated measure:
+	constituents_df_list = []
+	ports = crsp_comp.monthly_portfolio_sorts(db, d_mean_mon, variable_list, ncuts)
 	for variable in variable_list:
 		to_append = ports[variable]["ret"]
-		to_append["days"] = days
+		to_append["days"] = -99
 		to_append["variable"] = variable
 		port_sort_ret = port_sort_ret.append(to_append)
 
 		to_append = ports[variable]["bm"]
-		to_append["days"] = days
+		to_append["days"] = -99
 		to_append["variable"] = variable
 		port_sort_bm = port_sort_bm.append(to_append)
 
 		to_append = ports[variable]["op"]
-		to_append["days"] = days
+		to_append["days"] = -99
 		to_append["variable"] = variable
 		port_sort_op = port_sort_op.append(to_append)
 
-# Second, do the portfolios for aggregated measure:
-ports = crsp_comp.monthly_portfolio_sorts(db, d_mean_mon, variable_list, ncuts)
-for variable in variable_list:
-	to_append = ports[variable]["ret"]
-	to_append["days"] = -99
-	to_append["variable"] = variable
-	port_sort_ret = port_sort_ret.append(to_append)
+		to_append_const = ports[variable]["constituents"]
+		to_append_const["days"] = -99
+		to_append_const["variable"] = variable
+		to_append_const.columns = ["form_date", "permno", "port", "days", "variable"]
+		constituents_df_list.append(to_append_const)
 
-	to_append = ports[variable]["bm"]
-	to_append["days"] = -99
-	to_append["variable"] = variable
-	port_sort_bm = port_sort_bm.append(to_append)
+	# Merging all constituents lists together:
+	constituents_to_save = pd.concat(constituents_df_list)
 
-	to_append = ports[variable]["op"]
-	to_append["days"] = -99
-	to_append["variable"] = variable
-	port_sort_op = port_sort_op.append(to_append)
+	# Saving results:
+	print("Saving data")
+	port_sort_ret.to_csv("estimated_data/disaster_sorts/port_sort_ret_ind.csv")
+	port_sort_bm.to_csv("estimated_data/disaster_sorts/port_sort_bm_ind.csv")
+	port_sort_op.to_csv("estimated_data/disaster_sorts/port_sort_op_ind.csv")
 
-# Saving results:
-print("Saving data")
-port_sort_ret.to_csv("estimated_data/disaster_sorts/port_sort_ret_ind.csv")
-port_sort_bm.to_csv("estimated_data/disaster_sorts/port_sort_bm_ind.csv")
-port_sort_op.to_csv("estimated_data/disaster_sorts/port_sort_op_ind.csv")
+	# pivoting data for constituents to save:
+	constituents_to_save["port"] = constituents_to_save["port"].astype(float)
+	constituents_to_save = pd.pivot_table(
+		constituents_to_save, index = ["permno", "form_date"], value = "port", columns = ["variable", "days"])
+	constituents_to_save.columns = constituents_df.columns.to_flat_index()
+	constituents_to_save.to_csv("estimated_data/disaster_sorts/port_sort_const_ind.csv")
 
 
-# if __name__ == "__main__": sys.exit(main(sys.argv))
+if __name__ == "__main__": sys.exit(main(sys.argv))
